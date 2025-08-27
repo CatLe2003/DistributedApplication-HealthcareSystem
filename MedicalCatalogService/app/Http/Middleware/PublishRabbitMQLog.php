035d4b4d4102ecdea10214b7ecf5a564dff826bc
CatLe2003 <songcatlengo.learning@gmail.com>
2025-08-28 04:36:11 +0700

    Add RabbitMQ middleware to publish event log

---
+++ MedicalCatalogService/app/Http/Middleware/PublishRabbitMQLog.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Helpers\RabbitMQHelper;
+
+class PublishRabbitMQLog
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($request->is('api/*')) {
+
+            $status = $response->getStatusCode();
+            $level = 'INFO';
+
+            if ($status >= 400) {
+                $level = 'ERROR';
+            } 
+
+            RabbitMQHelper::publish('log_exchange', [
+                'event' => $request->method() . ' ' . $request->path(),
+                'service' => 'MedicalCatalogService',
+                'ipAddress' => $request->ip(),
+                'level' => $level,
+                'status' => $status,
+                'response' => $response,
+                'user' => $request->json('user') || null
+            ]);
+        }
+
+        return $response;
+    }
+}
